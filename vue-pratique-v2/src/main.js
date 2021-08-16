c38dc44f4e9a2794ecbf6ffcb62b232201dae975
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2021-08-16 17:51:31 +0200

    g

--- vue-pratique-v2/src/main.js
+++ vue-pratique-v2/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+//import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createApp } from 'vue'
 
-createApp(App).mount('#app')
+//Vue.config.productionTip = false
+
+createApp(App)
+    .use(router)
+    .mount('#app')
+
+/*new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')*/
